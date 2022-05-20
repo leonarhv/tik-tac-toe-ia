52f51f449719037a36aeb290b57b2ef2e3fcc02a
Leonardo Vieira <leohvir@gmail.com>
2022-05-19 23:41:41 -0300

    minimax starter implementation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const X = "X";
 const O = "O";
-let turn = X;
+let turn = O;
 let tiles = [
     [document.querySelector("#tile_1"), document.querySelector("#tile_2"), document.querySelector("#tile_3")],
     [document.querySelector("#tile_4"), document.querySelector("#tile_5"), document.querySelector("#tile_6")],
@@ -8,6 +8,69 @@ let tiles = [
 ]
 
 let hasWinner = false;
+
+const bestMove = () => {
+    let bestScore = -Infinity;
+    let move;
+    for (let i=0; i<3; i++) {
+        for (let j=0; j<3; j++) {
+            if (tiles[i][j].textContent == '') {
+                tiles[i][j].textContent = O;
+                let score = minimax(tiles, 0, false);
+                tiles[i][j].textContent = '';
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = { i, j };
+                }
+            }
+        }
+    }
+    tiles[move.i][move.j].textContent = O;
+    turn = X;
+    checkWinner();
+}
+
+let scores = {
+    'X': 1,
+    'O': -1,
+    'tie': 0
+}
+
+const minimax = (board, depth, isMaximizing) => {
+    let result = simpleCheckWinner();
+    if(result != null) {
+        return scores[result];
+    }
+
+    if(isMaximizing) {
+        return 1;
+        let bestScore = -Infinity;
+        for (let i=0; i<3; i++) {
+            for (let j=0; j<3; j++) {
+                if (board[i][j].textContent == '') {
+                    board[i][j].textContent = O;
+                    let score = minimax(board, depth + 1, false);
+                    board[i][j].textContent = '';
+                    bestScore = Math.max(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i=0; i<3; i++) {
+            for (let j=0; j<3; j++) {
+                if (board[i][j].textContent == '') {
+                    board[i][j].textContent = X;
+                    let score = minimax(board, depth + 1, true);
+                    board[i][j].textContent = '';
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+}
 
 const setTileContent = (event) => {
     if(!hasWinner) {
@@ -20,6 +83,7 @@ const setTileContent = (event) => {
             turn = X;
         }
         checkWinner();
+        bestMove();
     }
 }
 
@@ -28,17 +92,57 @@ const checkWinner = () => {
     let isOWinner = false;
 
     isXWinner = horizontalCheck(tiles, X) || verticalCheck(tiles, X) || diagonalCheck(tiles, X);
-    console.log(diagonalCheck(tiles, X));
     isOWinner = horizontalCheck(tiles, O) || verticalCheck(tiles, O) || diagonalCheck(tiles, O);
 
     if(isXWinner) {
         document.querySelector("#winner_text").textContent = "O X foi o vencedor dessa partida";
         hasWinner = true;
+        return O;
     }
     if(isOWinner) {
         document.querySelector("#winner_text").textContent = "O 'O' foi o vencedor dessa partida";
         hasWinner = true;
+        return X;
     }
+
+    if(isTie()) {
+        document.querySelector("#winner_text").textContent = "Deu velha";
+        return 'tie';
+    }
+
+    return null;
+}
+
+const simpleCheckWinner = () => {
+    let isXWinner = false;
+    let isOWinner = false;
+
+    isXWinner = horizontalCheck(tiles, X) || verticalCheck(tiles, X) || diagonalCheck(tiles, X);
+    isOWinner = horizontalCheck(tiles, O) || verticalCheck(tiles, O) || diagonalCheck(tiles, O);
+
+    if(isXWinner) {
+        return O;
+    }
+    if(isOWinner) {
+        return X;
+    }
+
+    if(isTie()) {
+        return 'tie';
+    }
+
+    return null;
+}
+
+const isTie = () => {
+    for (let i=0; i<3; i++) {
+        for (let j=0; j<3; j++) {
+            if (tiles[i][j].textContent == '') {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 const horizontalCheck = (tiles, TILE) => {
@@ -47,7 +151,10 @@ const horizontalCheck = (tiles, TILE) => {
         for(let j=0; j<3; j++) {
             if(tiles[i][j].textContent === TILE) {
                 sequence += 1;
-                if (sequence === 3) return true;
+                if (sequence === 3) {
+                    console.log('ganhou horizontal');
+                    return true
+                };
             }
         }
     }
@@ -63,6 +170,7 @@ const verticalCheck = (tiles, TILE) => {
             tiles[1][i].textContent === TILE &&
             tiles[2][i].textContent === TILE
         ) {
+            console.log('ganhou vertical');
             return true;
         }
     }
@@ -77,6 +185,7 @@ const diagonalCheck = (tiles, TILE) => {
         tiles[1][1].textContent === TILE &&
         tiles[2][2].textContent === TILE
     ) {
+        console.log('ganhou diagonal');
         return true;
     }
 
@@ -86,3 +195,5 @@ const diagonalCheck = (tiles, TILE) => {
 for (let i = 1; i <= 9; i++) {
     document.querySelector("#tile_" + i).addEventListener("click", setTileContent);
 }
+
+bestMove()
